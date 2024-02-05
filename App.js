1ad9bd3d99303b90b862a39fda9a3320504d58f8
PabloASanch <Pablosanchez.2@hotmail.com>
2024-02-05 14:01:43 -0700

    change

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import { NativeRouter, Route, Routes } from "react-router-native";
+import { Signin } from "./pages/signin";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NativeRouter>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+        </Routes>
+      </NativeRouter>
     </View>
   );
 }
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
