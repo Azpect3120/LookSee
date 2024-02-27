6bda34b3dff4e232ac10d450c17af7f59d1a5b91
PabloASanch <Pablosanchez.2@hotmail.com>
2024-02-27 13:01:37 -0700

    create

--- pages/create.js
+++ pages/create.js
@@ -9,10 +9,12 @@ import {
   Alert,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 import { useNavigate } from "react-router-native";
 import NavBar from "./components/NavBar";
 
 export const Create = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -20,8 +22,6 @@ export const Create = () => {
     file: null,
     type: "",
   });
-  const [currentStep, setCurrentStep] = useState(1);
-  const navigate = useNavigate();
 
   const pickMedia = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -32,18 +32,27 @@ export const Create = () => {
   
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Videos,
-      allowsEditing: true, 
+      allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
       videoMaxDuration: 60,
     }); 
-
     if (!result.canceled && result.assets[0].uri) {
-      setFormData({ ...formData, file: result.assets[0].uri, type: "video" });
-      setCurrentStep(currentStep + 1);
+      try {
+        console.log(`Fetching info for URI: ${result.assets[0].uri}`);
+        const fileInfo = await FileSystem.getInfoAsync(result.assets[0].uri);
+        if (fileInfo.size <= 52428800) { 
+          setFormData({ ...formData, file: result.assets[0].uri, type: "video" });
+        } else {
+          Alert.alert("File size exceeds 50MB. Please select a smaller file.");
+        }
+      } catch (error) {
+        console.error(`Error getting file info: ${error}`);
+        Alert.alert("Error", "Failed to get file information.");
+      }
     } else {
-      Alert.alert("Error", "No video was selected.");
-    }
+      Alert.alert("Error", "No video was selected or the video URI is unavailable.");
+    }    
   };
 
   const handleInputChange = (name, value) => {
@@ -51,69 +60,71 @@ export const Create = () => {
   };
 
   const handleSubmit = async () => {
-    console.log(formData);
-  };
+    const backendURL = "https://looksee.gophernest.net/posts";
+    let localUri = formData.file;
+    let filename = localUri.split("/").pop();
+    let type = `video/${filename.split('.').pop()}`;
 
-  const renderUploadVideoStep = () => (
-    <View style={styles.stepContainer}>
-      <TouchableOpacity onPress={pickMedia} style={styles.button}>
-        <Text style={styles.buttonText}>Pick a Video</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    let formDataBackend = new FormData();
+    formDataBackend.append("title", formData.title);
+    formDataBackend.append("text_content", `${formData.location} ${formData.description}`);
+    formDataBackend.append("address", "6997 N Glen Harbor Blvd");
+    formDataBackend.append("video_upload", { uri: localUri, name: filename, type });
+    formDataBackend.append("author_id",  "442e6b5a-ac9e-453e-8a9d-a052df9c169c");
 
-  const renderDetailsStep = () => (
-    <View style={styles.stepContainer}>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => handleInputChange("title", text)}
-        value={formData.title}
-        placeholder="Title"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => handleInputChange("description", text)}
-        value={formData.description}
-        placeholder="Description"
-      />
-      <TouchableOpacity onPress={() => setCurrentStep(currentStep + 1)} style={styles.button}>
-        <Text style={styles.buttonText}>Next</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  const renderLocationStep = () => (
-    <View style={styles.stepContainer}>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => handleInputChange("location", text)}
-        value={formData.location}
-        placeholder="Location"
-      />
-      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
-        <Text style={styles.buttonText}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  // Determine which step content to render
-  const renderStepContent = () => {
-    switch (currentStep) {
-      case 1:
-        return renderUploadVideoStep();
-      case 2:
-        return renderDetailsStep();
-      case 3:
-        return renderLocationStep();
-      default:
-        return <View />;
+    try {
+      let response = await fetch(backendURL, {
+        method: "POST",
+        body: formDataBackend,
+      });
+      if (response.ok) {
+        Alert.alert("Form submitted successfully");
+        navigate("/");
+      } else {
+        Alert.alert("Form submission failed", `Status Code: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Submission error:", error);
+      Alert.alert("Submission error", error.message);
     }
   };
 
+
   return (
     <>
+      <StatusBar barStyle="dark-content" />
       <View style={styles.container}>
-        {renderStepContent()}
+        <View style={styles.content}>
+          <TouchableOpacity onPress={pickMedia} style={styles.button}>
+            <Text style={styles.buttonText}>Pick a Video</Text>
+          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => handleInputChange("title", text)}
+            value={formData.title}
+            placeholder="Enter a Title"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => handleInputChange("description", text)}
+            value={formData.description}
+            placeholder="Description"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => handleInputChange("location", text)}
+            value={formData.location}
+            placeholder="Enter A Location"
+          />
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+        <NavBar
+          footerStyle={styles.footer}
+          footerTabStyle={styles.footerTab}
+          selected={styles.selected}
+        />
       </View>
     </>
   );
@@ -121,20 +132,18 @@ export const Create = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  stepContainer: {
     width: "100%",
+    flex: 1,
+    backgroundColor: "#f5f5f5",
     alignItems: "center",
     justifyContent: "center",
+  },
+  content: {
+    padding: 20,
   },
   input: {
     height: 40,
-    width: "100%",
-    marginVertical: 12,
+    margin: 12,
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
@@ -145,12 +154,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#007bff",
     padding: 10,
     borderRadius: 5,
-    marginVertical: 12,
-    width: "100%",
+    margin: 12,
     alignItems: "center",
   },
   buttonText: {
     color: "#fff",
     fontWeight: "bold",
+  },
+  footer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
+    height: "10%",
+    backgroundColor: "#000",
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  footerTab: {
+    fontSize: 20,
+    color: "#fff",
+    fontFamily: "Arvo",
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 15,
   },
 });
